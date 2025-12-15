62c42fe5ea07069f66621af128999a0cc4fe4955
Chaimaa101 <afkirchaimaa36@gmail.com>
2025-12-15 12:12:43 +0100

    first commit

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: 'gestion-produits-backend-production-0655.up.railway.app/api', 
 });
 
 export const getProducts = () => api.get('/products');
